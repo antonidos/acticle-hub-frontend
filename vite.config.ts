b033ff71180e56b7af28adb0904cb389e60619f9
antonidos <antonidos@users.noreply.github.com>
2025-07-12 15:06:02 +0400

    initialized prettier, css modules, redux

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      generateScopedName: '[local]__[hash:base64:5]',
+    },
+  },
+});
